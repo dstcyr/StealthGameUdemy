b0b6ec66eada372d32556acef45af2842b0d1479
David St-Cyr <st-cyrdavid@hotmail.com>
2024-06-10 11:42:03 -0400

    Update target.cs to suppress UE warnings

--- Source/FPSGame.Target.cs
+++ Source/FPSGame.Target.cs
@@ -8,8 +8,9 @@ public class FPSGameTarget : TargetRules
 	public FPSGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
-		ExtraModuleNames.AddRange( new string[] { "FPSGame" } );
+        ExtraModuleNames.AddRange( new string[] { "FPSGame" } );
 	}
 }
--- Source/FPSGameEditor.Target.cs
+++ Source/FPSGameEditor.Target.cs
@@ -8,8 +8,9 @@ public class FPSGameEditorTarget : TargetRules
 	public FPSGameEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
-		ExtraModuleNames.AddRange( new string[] { "FPSGame" } );
+        ExtraModuleNames.AddRange( new string[] { "FPSGame" } );
 	}
 }
